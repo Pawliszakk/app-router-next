c378dc3672f7628750bde874ff4832d32e503756
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-01-02 23:59:09 +0100

    working on food project

---
+++ app/community/page.jsx
@@ -0,0 +1,5 @@
+const CommunityPage = () => {
+	return <h1>community</h1>;
+};
+
+export default CommunityPage;
---
+++ app/meals/[slug]/page.jsx
@@ -0,0 +1,5 @@
+const MealDeatailPage = ({ params }) => {
+	return <h1>Meal: {params.slug}</h1>;
+};
+
+export default MealDeatailPage;
