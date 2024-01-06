27b297396a459db6ea86500a7c0e2fa938a3d78c
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-01-06 21:52:51 +0100

    added styles to header

---
+++ components/MainHeader.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import logoImg from '../assets/logo.png';
+import classes from './MainHeader.module.css';
+import Image from 'next/image';
+
+const MainHeader = () => {
+	return (
+		<header className={classes.header}>
+			<Link href="/" className={classes.logo}>
+				<Image
+					src={logoImg.src}
+					alt="A plate with food on it"
+					width={300}
+					height={300}
+                    priority
+				/>
+				NextLevel Food
+			</Link>
+
+			<nav className={classes.nav}>
+				<ul>
+					<li>
+						<Link href="/meals">Browse Meals</Link>
+					</li>
+					<li>
+						<Link href="/community">Community</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default MainHeader;
