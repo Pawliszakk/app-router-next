bcba5c7bdca93de3f4aa0600fff17f59710091ff
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-01-14 23:01:58 +0100

    ad

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
-import classes from './page.module.css';
 import ImageSlideShow from '../components/SlideShow/slideShow';
+import classes from './page.module.css';
 
 export default function Home() {
 	return (
