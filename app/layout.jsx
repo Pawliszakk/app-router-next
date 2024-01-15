8cde00d3ac820b1ec00f7de668e95ef33aea84bb
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-01-15 22:39:48 +0100

    changed description in main page

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import MainHeaderBackground from '../components/MainHeader/MainHeaderBackground'
 
 export const metadata = {
 	title: 'NextLevel Food',
-	description: 'Delicious meals, shared by a food-loving community.',
+	description: 'Delicious meals, shared by a food-loving community from all around the world.',
 };
 
 export default function RootLayout({ children }) {
